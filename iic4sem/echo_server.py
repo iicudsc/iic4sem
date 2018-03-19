b60530120b97d0dcf9b7b8d0e48c757cf5aa9d7e
Abhinav Saxena <xandfury@gmail.com>
2018-03-20 01:05:55 +0530

    add basic servers, intial release

--- iic4sem/echo_server.py
+++ iic4sem/echo_server.py
@@ -1,19 +1,57 @@
+"""
+Simple Echo Server capable of handling only one client at a time.
+"""
 import socket
+import sys
+import logging
+logger = logging.getLogger(__name__)
+
+# For debugging --
+# import logging as logger
+# logger.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 class EchoServer:
     def __init__(self):
-        pass
+        logger.info('Initializing Simple Echo Server!')
+        self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def handler():
+    # our connection handler.
+    def handler(self, client_sock, client_address):
         """Handles the incoming client connection"""
+        logger.info('client connected: ', client_address)
+        while True:
+            data = client_sock.recv(1024)
+            logger.info('Received data: ', data)
+            if data:
+                client_sock.sendall(data)
+            else:
+                break
 
     def start(self, host, port):
-        """Starts the Echo Web server"""
-        pass
+        """Starts the Echo server"""
+        conn = (host, port)
+        # TCP connection
+        self.listener.bind(conn)
+        # how many connections do we want to allow? This would go in sock.listen
+        self.listener.listen(16)
+        while True:
+            logger.info('EchoServer waiting for a connection on {}'.format(conn))
+            try:
+                client_sock, client_address = self.listener.accept()
+                self.handler(client_sock, client_address)
+            finally:
+                self.stop()
 
     def stop(self):
         """Stops the Echo Server"""
-        pass
+        # Maybe shutdown the socket?
+        self.listener.close()
 
+# For infoging
 if __name__ == '__main__':
-    pass
+    test = EchoServer()
+    try:
+        logger.info('Starting Echo Server!')
+        test.start('', 4443)
+    except KeyboardInterrupt:
+        logger.info('Stopping Echo Server!')
--- setup.py
+++ setup.py
@@ -1,33 +1,34 @@
 import multiprocessing
-from ez_setup import use_setuptools
-use_setuptools()
 from setuptools import setup, find_packages
 import iic4sem
 
 setup(
-    name='IIC4Sem',
+    name='iic4sem',
     version=iic4sem.__version__,
     packages=find_packages(exclude=['*.pyc']),
     scripts=['bin/iic4sem'],
-    url="https://iic.ac.in",
-    license='GPL 2',
+    license='GPL 3',
     author="Abhinav Saxena",
     author_email="abhinav.saxena@iic.ac.in",
     classifiers=[
-        "Development Status :: 1 - Alpha",
+        "Development Status :: 3 - Alpha",
         "Environment :: Console",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Networking",
     ],
+    project_urls={
+        'Funding': 'http://iic.du.ac.in',
+    },  
     keywords="IIC UDSC",
     include_package_data=True,
     long_description=open('README.md').read(),
-    description="""Basic Network Programming Assignments For IIC, UDSC 4 Sem Final""",
+    description="Basic Network Programming Assignments For IIC, UDSC 4 Sem Final",
     test_suite='nose.collector',
     tests_require="nose",
-    install_requires=open('requirements.txt').read().splitlines(),
+    install_requires=open('requirements.txt').read().splitlines()
 )
